0325777cd00d7821411d414e7865c59f46a2d9ec
Mr-Babarkhan <100486385+Mr-Babarkhan@users.noreply.github.com>
2022-12-27 21:11:08 -0800

    First project of typescript

---
+++ index.ts
@@ -0,0 +1,86 @@
+#! /usr/bin/env node
+
+import inquirer  from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation"
+import { type } from "os";
+
+async function sleep() {
+    return new Promise((res)=>{
+        setTimeout(res,2000);
+    })
+}
+async function welcome() {
+    let rainbow = chalkAnimation.rainbow('lets calculation');
+    await sleep();
+    rainbow.stop();
+    console.log(` _____________________
+    |  _________________  |
+    | | Result          | |
+    | |_________________| |
+    |  ___ ___ ___   ___  |
+    | | 7 | 8 | 9 | | + | |
+    | |___|___|___| |___| |
+    | | 4 | 5 | 6 | | - | |
+    | |___|___|___| |___| |
+    | | 1 | 2 | 3 | | x | |
+    | |___|___|___| |___| |
+    | | . | 0 | = | | / | |
+    | |___|___|___| |___| |
+    |_____________________|
+    `)
+}
+
+async function askQuestion() {
+    const answers =await inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+        type:"list",
+        name:"operator",
+        message:"Which operation you want to perform",
+        choices:["Addition","Subtraction","Multiplication","Division"]
+    },
+    {
+        type:"number",
+        name:"num1",
+        message:"Enter your first number ="
+    },
+    {
+        type:"number",
+        name:"num2",
+        message:"Enter your second number = "
+    }
+  ])
+  if (answers.operator== "Addition") {
+              console.log(chalk.green(`${answers.num1} + ${answers.num2} = Answer is   ${answers.num1 + answers.num2}`));
+    }
+    else if(answers.operator=="Subtraction"){
+        console.log(chalk.green(`${answers.num1} - ${answers.num2} = Answer is ${answers.num1 - answers.num2} `));  
+    }
+    else if(answers.operator=="Multiplication"){
+        console.log(chalk.green(`${answers.num1} * ${answers.num2} = Answer is ${answers.num1 * answers.num2}`));  
+    }
+    else if(answers.operator=="Division"){
+        console.log(chalk.green(`${answers.num1} / ${answers.num2} = Answer is ${answers.num1 / answers.num2}`));  
+    }
+}
+
+await welcome();
+// askQuestion();
+
+async function startAgain(){
+    do{
+        await askQuestion();
+       var again = await inquirer
+        .prompt({
+            type:"input",
+            name:"restart",
+            message:"Do you want to continue? press y or n: "
+        });
+} 
+while(again.restart== 'y' || again.restart== 'Y' || again.restart== 'yes' || again.restart== 'YES')
+}  
+    
+startAgain();
+
